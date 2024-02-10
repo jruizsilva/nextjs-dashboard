11d86fee83bf93c28365bdfd3e9a57aca42a341a
jruizsilva <ruizsilvajonathan@gmail.com>
2024-02-10 18:28:42 -0300

    active link style with clsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+import { inter } from './ui/fonts';
+import './ui/global.css';
+
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
